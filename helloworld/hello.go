ee706a19759aba227bcf819b11b8374598d80359
gjyoung1974 <gjyoung1974@gmail.comm>
2018-10-03 17:10:08 -0700

    new file:   .idea/appengine.iml 	new file:   .idea/dataSources.local.xml 	new file:   .idea/dataSources.xml 	new file:   .idea/dataSources/61f3565c-1a75-4d43-9647-9eb72d7a60d2.xml 	new file:   .idea/dataSources/dace3183-646e-42a1-8118-bf170e01428c.xml 	new file:   .idea/dataSources/dace3183-646e-42a1-8118-bf170e01428c/storage_v2/_src_/schema/main.uQUzAA.meta 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	modified:   helloworld/hello.go

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,21 +1,87 @@
-// Copyright 2018 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"google.golang.org/appengine"
+	"github.com/jinzhu/gorm"
+	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	http.HandleFunc("/", handle)
-	appengine.Main()
+var db *gorm.DB
+var err error
+
+type Person struct {
+	ID        uint   `json:”id”`
+	FirstName string `json:”firstname”`
+	LastName  string `json:”lastname”`
+	City      string `json:”city”`
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+func main() {
+
+	// NOTE: See we’re using = to assign the global var
+	// instead of := which would assign it only in this function
+	db, err = gorm.Open("sqlite3", "./people.db")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer db.Close()
+	db.AutoMigrate(&Person{})
+	r := gin.Default()
+	r.GET("/people /", GetPeople)
+	r.GET("/people /:id", GetPerson)
+	r.POST("/people", CreatePerson)
+	r.PUT("/people /:id", UpdatePerson)
+	r.DELETE("/people /:id", DeletePerson)
+	r.Run(":8080")
 }
+func DeletePerson(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var person Person
+	d := db.Where("id = ?", id).Delete(&person)
+	fmt.Println(d)
+	c.JSON(200, gin.H{"id #" + id: "deleted"})
+}
+func UpdatePerson(c *gin.Context) {
+	var person Person
+	id := c.Params.ByName("id")
+	if err := db.Where("id = ?", id).First(&person).Error;
+		err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	}
+	c.BindJSON(&person)
+	db.Save(&person)
+	c.JSON(200, person)
+}
+func CreatePerson(c *gin.Context) {
+	var person Person
+	c.BindJSON(&person)
+	db.Create(&person)
+	c.JSON(200, person)
+}
+func GetPerson(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var person Person
+	if err := db.Where("id = ?", id).First(&person).Error;
+		err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, person)
+	}
+}
+func GetPeople(c *gin.Context) {
+	var people []Person
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
+}
+
+// curl -d '{"FirstName":"Gordon", "LastName":"Young", "City":"Gilbert"}' -H "Content-Type: application/json" -X POST http://localhost:8080/people
